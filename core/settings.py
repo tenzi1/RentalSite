870c2c9e410ca7a7e68c7e36c8a1272a16385db2
tshering <sherpatshering783@gmail.com>
2024-04-01 18:13:36 +0545

    feat: Add static file configuration

--- core/settings.py
+++ core/settings.py
@@ -141,3 +141,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+# static file settings
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
