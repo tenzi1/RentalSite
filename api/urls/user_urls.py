fd242dbf754063bd821e0f358a495b2109eddf95
tshering <sherpatshering783@gmail.com>
2024-06-26 17:58:12 +0545

    feat: Add user detail for chat layout

--- api/urls/user_urls.py
+++ api/urls/user_urls.py
@@ -11,4 +11,11 @@ router = routers.DefaultRouter()
 router.register(r"users", user_viewsets.UserViewSet)
 router.register(r"groups", user_viewsets.GroupViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+    path(
+        "chat_user/<int:pk>/",
+        user_viewsets.ChatUserDetailView.as_view(),
+        name="chat-user-detail",
+    ),
+]
