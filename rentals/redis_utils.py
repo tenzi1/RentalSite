2f93e8cc1d66f24bc2a097666e5e429a1fed1310
tshering <sherpatshering783@gmail.com>
2024-08-01 18:14:26 +0545

    fix: Add redis client

--- rentals/redis_utils.py
+++ rentals/redis_utils.py
@@ -7,8 +7,6 @@ import redis
 def get_redis_client():
     REDIS_HOST = os.environ.get("REDIS_HOST")
     REDIS_PORT = os.environ.get("REDIS_PORT")
+    REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
 
-    return redis.Redis(
-        host=REDIS_HOST,
-        port=REDIS_PORT,
-    )
+    return redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
